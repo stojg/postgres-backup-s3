6c2413d64c336fea355d2f771f65afa85fb952d5
Stig Lindqvist <me@stig.nz>
2022-06-17 11:31:41 +1200

    add runtime metrics to logs

--- backup.sh
+++ backup.sh
@@ -4,6 +4,8 @@ set -e
 set -u
 set -o pipefail
 
+start=`date +%s`
+
 if [ "${S3_ACCESS_KEY_ID}" = "**None**" ]; then
   echo "You need to set the S3_ACCESS_KEY_ID environment variable."
   exit 1
@@ -60,21 +62,24 @@ POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTG
 
 TIME_STAMP=$(date +"%Y-%m-%dT%H:%M:%SZ")
 echo "checking connection to bucket.."
-echo 'backup running' | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_${TIME_STAMP}.log
+printf "$start\n" | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_${TIME_STAMP}.log
 echo "access to bucket confirmed"
 
 echo "creating dump of database '${POSTGRES_DATABASE}' from '${POSTGRES_HOST}'..."
-
 set -x
 pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_${TIME_STAMP}.dump
 set +x
 
-echo "database backed up and stored in bucket"
+end=`date +%s`
+runtime=$((end-start))
 
+printf "$start\t$runtime\n" | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_${TIME_STAMP}.log
+
+echo "database backed up and stored in bucket"
 echo "copying to latest"
 
 aws $AWS_ARGS s3 cp s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_${TIME_STAMP}.dump s3://$S3_BUCKET/$S3_PREFIX/latest.dump
-date '+%s' | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/latest.log
+printf "$start\t$runtime\n" | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/latest.log
 
 echo
 echo "everything is peachy, have a good day"
